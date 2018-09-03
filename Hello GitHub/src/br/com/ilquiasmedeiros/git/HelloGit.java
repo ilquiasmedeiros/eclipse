eae930c9e6f9e3e442e7988ca200c52bd930abb4
ilquiasmedeiros <42920403+ilquiasmedeiros@users.noreply.github.com>
2018-09-03 13:15:30 -0300

    Adicionado Projeto ao GitHub

---
+++ Hello GitHub/src/br/com/ilquiasmedeiros/git/HelloGit.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.com.ilquiasmedeiros.git;
+
+/**
+ * @author ilquias.medeiros
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+
+}
